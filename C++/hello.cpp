ce4dd4d757d52aec9fb2bca26a8275d4ae99af17
linhnvfpt <vanlinh34@gmail.com>
2016-08-27 10:50:45 +0700

    Update file hello.cpp - Print name company.

--- C++/hello.cpp
+++ C++/hello.cpp
@@ -7,5 +7,6 @@ int main(int argc, char** argv)
     cout << "Hello World" << endl;
 	cout << "LinhNV" << endl;
 	cout << "Ha Noi university of technology!";
+	cout << "FPT company" << endl;
     return 0;	
 }
