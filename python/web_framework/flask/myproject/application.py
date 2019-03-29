c4a0553d8c55b59f95fb19d9d89ff06a17b4f7e5
Linh <linh_nv@ehicas.com.vn>
2019-03-29 14:33:44 +0700

    update code

---
+++ python/web_framework/flask/myproject/application.py
@@ -0,0 +1,36 @@
+from flask import Flask, url_for, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Home Page"
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello_world(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % subpath
+
+@app.route('/login')
+def login():
+    return 'login'
+
+""" with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('show_user_profile', username="John Doe")) """
