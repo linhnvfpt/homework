e4f0d993be387a3ab6cb3519e816a2f2881f9eb9
Van Linh <vanlinh34@gmail.com>
2019-05-08 06:45:16 +0700

    Update file sneak_preview.py

---
+++ python/app_python/sneak_preview.py
@@ -0,0 +1,28 @@
+bob = ['bob smith', 42, 3000, 'software']
+sue = ['sue jones', 45, 4000, 'hardware']
+
+people = [bob, sue]
+for person in people:
+    print(person)
+
+print(people[1][0])
+
+for person in people:
+    print(person[0].split())
+    person[2] *= 1.20
+
+for person in people:
+    print(person[2])
+
+pays = [person[2] for person in people]
+print(pays)
+
+pays1 = map((lambda x: x[2]), people)
+print(pays1)
+
+print(type(sum(person[2] for person in people)))
+
+people.append(['tom', 50, 0, None])
+print(people[-1][0])
+print(people[-2][0])
+print(people[-3][0])
