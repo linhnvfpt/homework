66446a817c303b2e377379abcdea88af5e72b439
Linh <linh_nv@ehicas.com.vn>
2019-05-31 08:40:21 +0700

    update code

---
+++ c#/MVVMDemo/WPFDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFDataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person = new Person { Name = "Salman", Age = 26 };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = person;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = person.Name + " is " + person.Age;
+            MessageBox.Show(message);
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        private double ageValue;
+
+        public double Age
+        {
+            get { return ageValue; }
+
+            set
+            {
+                if (value != ageValue)
+                {
+                    ageValue = value;
+                }
+            }
+        }
+
+    }
+}
