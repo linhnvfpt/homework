ea50f9b8336f48f8cdd936ba3580ff304121cd95
Linh <linh_nv@ehicas.com.vn>
2020-02-15 11:02:45 +0700

    update python execises

---
+++ python/w3resource/python-execises/part-I/6.py
@@ -0,0 +1,6 @@
+sample_data = input('Input a sequence of comma-separated numbers: ')
+list_data = [x for x in sample_data.split(',')]
+tuple_data = tuple(list_data)
+print(list_data)
+print(tuple_data)
+
---
+++ python/w3resource/python-execises/part-I/7.py
@@ -0,0 +1,3 @@
+file_name = input('Input a file name: ')
+extension_file = file_name.split('.').pop()
+print(extension_file)
