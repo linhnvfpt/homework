1f0c380a412dee265f3a534650b3bbe9a81421ee
Linh <linh_nv@ehicas.com.vn>
2020-02-18 10:58:17 +0700

    update code

---
+++ python/w3resource/python-execises/part-I/37.py
@@ -0,0 +1,7 @@
+# Write a Python program to display your details like name, age, address in three different lines
+def personal_details():
+    name, age = "Simon", 19
+    address = "Ha Noi, Viet Nam"
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+
+personal_details()
