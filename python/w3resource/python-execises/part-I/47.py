e752447e2592baebc4869e3ba3a2112cd6d22408
Linh <linh_nv@ehicas.com.vn>
2020-02-19 15:11:51 +0700

    update code

--- python/w3resource/python-execises/part-I/47.py
+++ python/w3resource/python-execises/part-I/47.py
@@ -1,1 +1,3 @@
-
+# Write a Python program to find out the number of CPUs using
+import multiprocessing
+print(multiprocessing.cpu_count())
--- python/w3resource/python-execises/part-I/49.py
+++ python/w3resource/python-execises/part-I/49.py
@@ -1,1 +1,5 @@
-
+# Write a Python program to list all files in a directory in Python
+from os import listdir, getcwd
+from os.path import isfile, join
+files_list = [f for f in listdir(getcwd()) if isfile(join(getcwd(),f))]
+print(files_list)
