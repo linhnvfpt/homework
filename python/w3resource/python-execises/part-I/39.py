4aee43cfc5754e28dd59c193dc10d4add5d2e678
Linh <linh_nv@ehicas.com.vn>
2020-02-19 11:00:44 +0700

    update code

---
+++ python/w3resource/python-execises/part-I/39.py
@@ -0,0 +1,9 @@
+﻿# Write a Python program to compute the future value of a specified principal amount,
+# rate of interest, and a number of years.
+
+amt = 10000
+interest = 3.5
+years = 7
+
+future_value = amt * ((1 + ( 0.01 * interest )) ** years)
+print(round(future_value,2))
