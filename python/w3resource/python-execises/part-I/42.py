4ac4649046c64e5500f2183fdbae31c53f967193
Linh <linh_nv@ehicas.com.vn>
2020-02-19 11:32:58 +0700

    update code python execises

---
+++ python/w3resource/python-execises/part-I/42.py
@@ -0,0 +1,6 @@
+﻿# Write a Python program to determine if a Python shell 
+# is executing in 32bit or 64bit mode on OS
+
+# For 32 bit is will return 32 and for 64 bit it will return 64
+import struct
+print(struct.calcsize("P") * 8)
