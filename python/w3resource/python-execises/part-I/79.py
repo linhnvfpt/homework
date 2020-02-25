c26859a126870273f31974f463cfca19d03aef06
Linh <linh_nv@ehicas.com.vn>
2020-02-25 10:22:03 +0700

    update code python

--- python/w3resource/python-execises/part-I/79.py
+++ python/w3resource/python-execises/part-I/79.py
@@ -1,1 +1,30 @@
+# Write a Python program to get the size of an object in bytes
 
+import sys
+str1 = "one"
+str2 = "four"
+str3 = "three"
+
+class Animal:
+    def __init__(self, type, action):
+        self.type = type
+        self.action = action
+    def display():
+        print("Type {} and Action {}".format(self.type, self.action))
+
+class Dog(Animal):
+    def __init__(self, type, action):
+        Animal.__init__(self,type,action)
+        self.status = "Sleep"
+
+a = Animal("dog","sua")
+b = Animal("vitnnnnnnnnnnnnnnnn","quacnnnnnnnnnnnnnnn")
+c = Dog("lasa","sua")
+
+print()
+print(sys.getsizeof(str1))
+print(sys.getsizeof(str2))
+print(sys.getsizeof(str3))
+print(sys.getsizeof(a))
+print(sys.getsizeof(b))
+print(sys.getsizeof(c))
