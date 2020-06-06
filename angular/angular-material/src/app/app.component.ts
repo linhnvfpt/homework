df26741d0e2488a0c6a27926a84c8141bf275712
Linh <linh_nv@ehicas.com.vn>
2020-06-06 11:47:36 +0700

    update code

--- angular/angular-material/src/app/app.component.ts
+++ angular/angular-material/src/app/app.component.ts
@@ -5,7 +5,7 @@ import {
   MAT_DIALOG_DATA,
 } from '@angular/material/dialog';
 import { ChoosenComponentComponent } from './choosen-component/choosen-component.component';
-import { data } from './data_state_checkbox';
+import { data, data_state_checkbox } from './data_state_checkbox';
 
 export interface Element {
   mainsize: number;
@@ -56,9 +56,10 @@ const BRANCH_PIPE_DATA: Element[] = [
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  cell_content: string = 'R,O,C,S';
+  cell_content: string = 'R O C S';
   isMouseDown: boolean = false;
   data = data[0];
+  temp:data_state_checkbox;
 
   displayedColumns: string[] = [
     'Inch',
@@ -136,26 +137,26 @@ export class AppComponent implements OnInit {
     });
 
     if (count_R == 0) this.data.full_R = false;
-    else if (count_R == els_highlight.length) this.data.full_R = true;
-    else if (count_R < els_highlight.length)  this.data.undefine_R = true;
+    else if (count_R == els_highlight.length) { this.data.full_R = true; this.data.undefine_R = false; }
+    else if (count_R < els_highlight.length)  { this.data.undefine_R = true; }
 
     if (count_O == 0) this.data.full_O = false;
-    else if (count_O == els_highlight.length) this.data.full_O = true;
-    else if (count_O < els_highlight.length)  this.data.undefine_O = true;
+    else if (count_O == els_highlight.length) { this.data.full_O = true; this.data.undefine_O = false; }
+    else if (count_O < els_highlight.length)  { this.data.undefine_O = true; }
 
     if (count_C == 0) this.data.full_C = false;
-    else if (count_C == els_highlight.length) this.data.full_C = true;
-    else if (count_C < els_highlight.length)  this.data.undefine_C = true;
+    else if (count_C == els_highlight.length) { this.data.full_C = true; this.data.undefine_C = false;}
+    else if (count_C < els_highlight.length)  { this.data.undefine_C = true; }
 
     if (count_S == 0) this.data.full_S = false;
-    else if (count_S == els_highlight.length) this.data.full_S = true;
-    else if (count_S < els_highlight.length)  this.data.undefine_S = true;
-    console.log(this.data);
+    else if (count_S == els_highlight.length) { this.data.full_S = true; this.data.undefine_S = false; }
+    else if (count_S < els_highlight.length)  { this.data.undefine_S = true; }
+    
     // Open dialog with parameters
     const dialogRef = this.dialog.open(ChoosenComponentComponent, {
       width: '250px',
       height: '170px',
-      data: { data: this.data },
+      data: this.data,
     });
 
     // Process data after dialog closed
@@ -164,34 +165,37 @@ export class AppComponent implements OnInit {
       // Remove class "highlighted" out of cell
       let els = document.querySelectorAll('.highlighted');
       els.forEach.call(els, function (el) {
-        this.data = result;
         let content:string = el.innerHTML;
-        if (this.data.full_R) {
-          if (content.indexOf('R') == -1) content = 'R,' + content;
+        if (result.full_R) {
+          if (content.indexOf('R') == -1) content = 'R ' + content;
         }
         else {
-          if (content.indexOf('R') > -1) content = content.replace('R','');
+          // if checkbox R has status empty, no full, no indeterminate and content has 'R' => replace 'R'
+          if (content.indexOf('R') > -1 && result.undefine_R == false) content = content.replace('R','');
         }
 
-        if (this.data.full_O) {
-          if (content.indexOf('O') == -1) content = 'O,' + content;
+        if (result.full_O) {
+          if (content.indexOf('O') == -1) content = 'O ' + content;
         }
         else {
-          if (content.indexOf('O') > -1) content = content.replace('O','');
+          // if checkbox O has status empty, no full, no indeterminate and content has 'O' => replace 'O'
+          if (content.indexOf('O') > -1 && result.undefine_O == false) content = content.replace('O','');
         }
 
-        if (this.data.full_C) {
-          if (content.indexOf('C') == -1) content = 'C,' + content;
+        if (result.full_C) {
+          if (content.indexOf('C') == -1) content = 'C ' + content;
         }
         else {
-          if (content.indexOf('C') > -1) content = content.replace('C','');
+          // if checkbox C has status empty, no full, no indeterminate and content has 'C' => replace 'C'
+          if (content.indexOf('C') > -1 && result.undefine_C == false) content = content.replace('C','');
         }
 
-        if (this.data.full_S) {
-          if (content.indexOf('S') == -1) content = 'S,' + content;
+        if (result.full_S) {
+          if (content.indexOf('S') == -1) content = 'S ' + content;
         }
         else {
-          if (content.indexOf('S') > -1) content = content.replace('S','');
+          // if checkbox S has status empty, no full, no indeterminate and content has 'S' => replace 'S'
+          if (content.indexOf('S') > -1 && result.undefine_S == false) content = content.replace('S',''); 
         }
 
         el.innerHTML = content;
@@ -206,8 +210,20 @@ export class AppComponent implements OnInit {
       // Click mouse left
       this.isMouseDown = !this.isMouseDown;
       let element: HTMLTableCellElement = event.srcElement as HTMLTableCellElement;
+
+      // let els = document.querySelectorAll('.highlighted');
+      // els.forEach.call(els, function (el) {
+      //   el.classList.remove('highlighted');
+      // })
       if (element.tagName.toLowerCase() === 'td') {
-        if (this.isMouseDown) element.classList.add('highlighted');
+        if (this.isMouseDown)
+        {
+          if(element.classList.contains('highlighted'))
+            element.classList.remove('highlighted');
+          else
+            element.classList.add('highlighted');
+        }
+         
       }
     }
     return false;
--- angular/angular-material/src/app/choosen-component/choosen-component.component.ts
+++ angular/angular-material/src/app/choosen-component/choosen-component.component.ts
@@ -13,14 +13,17 @@ import { data_state_checkbox } from '../data_state_checkbox';
   styleUrls: ['./choosen-component.component.css'],
 })
 export class ChoosenComponentComponent implements OnInit {
+  isR = false;
+  isO = false;
+  isC = false;
+  isS = false;
 
   constructor(
     public dialogRef: MatDialogRef<ChoosenComponentComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: data_state_checkbox
+    @Inject(MAT_DIALOG_DATA) public dat: data_state_checkbox
   ) {}
 
   ngOnInit(): void {
-    console.log("Ben dialog", this.data);
     let element = document.querySelector('#cellSelected');
     const rect: DOMRect = element.getBoundingClientRect();
 
@@ -30,19 +33,51 @@ export class ChoosenComponentComponent implements OnInit {
       left: (rect.right - 20).toString() + 'px',
     };
     this.dialogRef.updatePosition(matDialogConfig.position);
+    this.isR = this.dat.full_R;
+    this.isO = this.dat.full_O;
+    this.isC = this.dat.full_C;
+    this.isS = this.dat.full_S;
   }
 
   public switchStatus(event: MatCheckboxChange, comp: string) {
     if (event.checked) {
-      if (comp === 'R') this.data.full_R = true;
-      else if (comp === 'O') this.data.full_O = true;
-      else if (comp === 'C') this.data.full_C = true;
-      else this.data.full_S = true;
+      if (comp === 'R') { 
+        this.dat.full_R = true;
+        this.isR = true;
+      }
+      else if (comp === 'O') {
+        this.dat.full_O = true;
+        this.isO = true;
+      }
+      else if (comp === 'C') { 
+        this.dat.full_C = true;
+        this.isC = true;
+      }
+      else {
+        this.dat.full_S = true;
+        this.isS = true;
+      }
     } else {
-      if (comp === 'R') this.data.full_R = false;
-      else if (comp === 'O') this.data.full_O = false;
-      else if (comp === 'C') this.data.full_C = false;
-      else this.data.full_S = false;
+      if (comp === 'R') { 
+        this.dat.full_R = false;
+        this.dat.undefine_R = false;
+        this.isR = false;
+      }
+      else if (comp === 'O') { 
+        this.dat.full_O = false;
+        this.dat.undefine_O = false;
+        this.isO = false;
+      }
+      else if (comp === 'C') { 
+        this.dat.full_C = false;
+        this.dat.undefine_C = false;
+        this.isC = false;
+      }
+      else {
+         this.dat.full_S = false;
+         this.dat.undefine_S = false;
+         this.isS = false;
+      }
     }
   }
 }
